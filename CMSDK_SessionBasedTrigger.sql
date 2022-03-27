4898b41b28e90f4ec4fe207599f9e9341ff021d7
PankajSharma <pankajeshwara@gmail.com>
2022-03-28 04:36:46 +1300

    Add files via upload

--- CMSDK_SessionBasedTrigger.sql
+++ CMSDK_SessionBasedTrigger.sql
@@ -1,3 +1,9 @@
+-- This script contains the DBFS user-session based trigger implemented within Oracle CMSDK. On each CMSDK user session creation and termination, the session-based trigger is used to call decryption and encryption procedure respectively. 
+
+-- Those two procedures first obtain the single-user as well as the multi-user GIS files to which user has privilege, and thencalls either the decryption or the encryption procedure so that these files can be decrypted or encrypted. 
+
+-- To prevent the execution of this trigger when multiple users are using the same account, which would corrupt the file resulting from double encryption and decryption, the trigger first queries the number of existing sessions of the user; only if no existing session of the user exists, then the files are decrypted or encrypted.
+
 CREATE OR REPLACE TRIGGER dec_enc_spatial_files
  BEFORE INSERT OR DELETE ON ifssys.odmz_session
  FOR EACH ROW
