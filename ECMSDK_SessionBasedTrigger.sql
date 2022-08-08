5817c802bdebd487aacbdfe8e1cf1b9c5097f653
PankajSharma <pankajeshwara@gmail.com>
2022-08-09 03:14:19 +1200

    Update ECMSDK_SessionBasedTrigger.sql

--- ECMSDK_SessionBasedTrigger.sql
+++ ECMSDK_SessionBasedTrigger.sql
@@ -13,22 +13,24 @@
 
 CREATE OR REPLACE TRIGGER dec_enc_spatial_files
  BEFORE INSERT OR DELETE ON ecmsdk.odmz_session
- FOR EACH ROW
+ FOR EACH ROW 
  DECLARE
  v_sessions NUMBER;
+ 
  CURSOR active_ECMSDK_user_sessions IS
  SELECT USERID FROM ECMSDK.ODMZ_SESSION
- WHERE USERID = (:NEW.USERID) OR USERID = (:OLD.USERID);
+   WHERE USERID = (:NEW.USERID) OR USERID = (:OLD.USERID);
+ 
  BEGIN
  OPEN active_ECMSDK_user_sessions;
  FETCH active_ECMSDK_user_sessions INTO v_sessions;
  -- Only if no existing sessions of this user, then decrypt or encrypt
  IF active_ECMSDK_user_sessions%NOTFOUND THEN
- IF INSERTING THEN
- CMSDK_spatial_crypt.get_11gecmsdk_user_files_dec (:NEW.USERID);
- ELSIF DELETING THEN
- CMSDK_spatial_crypt.get_11gecmsdk_user_files_enc (:OLD.USERID);
- END IF;
+   IF INSERTING THEN
+      ECMSDK_spatial_crypt.get_11gecmsdk_user_files_dec (:NEW.USERID);
+   ELSIF DELETING THEN
+      ECMSDK_spatial_crypt.get_11gecmsdk_user_files_enc (:OLD.USERID);
+   END IF;
  END IF;
  CLOSE active_ECMSDK_user_sessions;
 END dec_enc_spatial_files;
